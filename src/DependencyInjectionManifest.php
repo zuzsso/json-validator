b9a25ceca570bc9e084123a3aacb3a42b6234f49
Nicky Santamaria <ndsans@gmail.com>
2024-04-21 18:48:15 +0100

    initial commit

--- src/DependencyInjectionManifest.php
+++ src/DependencyInjectionManifest.php
@@ -2,18 +2,15 @@
 
 declare(strict_types=1);
 
-namespace YourProject;
+namespace JsonValidator;
 
 use DiManifest\AbstractDependencyInjection;
-use YourProject\HelloWorld\HelloWorldDiDefinitions;
 
 class DependencyInjectionManifest extends AbstractDependencyInjection
 {
     public static function getDependencies(): array
     {
         return array_merge(
-            HelloWorldDiDefinitions::getDependencies(),
-            // Add any other dependency definitions according to php-di/php-di, that need to be published
         );
     }
 }
--- tests/CustomTestCase.php
+++ tests/CustomTestCase.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace YourProject\Tests;
+namespace JsonValidator\Tests;
 
 use DI\Container;
 use PHPUnit\Framework\TestCase;
