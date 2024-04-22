135d30d8776ced461240f3ab0d1474a38ff15497
Nicky Santamaria <ndsans@gmail.com>
2024-04-22 20:51:33 +0100

    initial commit

--- tests/ValueStringCheckerTest.php
+++ tests/ValueStringCheckerTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace JsonValidator\Tests;
 
+use JsonValidator\Exception\StringValueNotAnEmailException;
+use JsonValidator\Exception\ValueStringEmptyException;
 use JsonValidator\Service\ValueStringChecker;
 
 class ValueStringCheckerTest extends CustomTestCase
@@ -14,5 +16,54 @@ class ValueStringCheckerTest extends CustomTestCase
     {
         parent::setUp();
         $this->sut = new ValueStringChecker();
+    }
+
+    public function shouldFailEmailValidationDataProvider(): array
+    {
+        $m1 = 'Expected a string, but got null or empty string';
+        $m2 = "The value 'blah' is meant to represent an email address, but it doesn't";
+
+        return [
+            ['blah', false, StringValueNotAnEmailException::class, $m2],
+            ['blah', true, StringValueNotAnEmailException::class, $m2],
+            ['', true, ValueStringEmptyException::class, $m1],
+
+        ];
+    }
+
+    /**
+     * @dataProvider shouldFailEmailValidationDataProvider
+     * @throws StringValueNotAnEmailException
+     * @throws ValueStringEmptyException
+     */
+    public function testShouldFailEmailValidation(
+        ?string $value,
+        bool $required,
+        string $expectedException,
+        string $expectedExceptionMessage
+    ): void {
+        $this->expectException($expectedException);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+        $this->sut->isEmailAddress($value, $required);
+    }
+
+
+    public function shouldAcceptEmailValidationDataProvider(): array
+    {
+        return [
+            [null, false],
+            ['', false],
+            ['a@b.com', true],
+            ['a@b.com', false],
+        ];
+    }
+
+    /**
+     * @dataProvider shouldAcceptEmailValidationDataProvider
+     */
+    public function testShouldAcceptEmailValidation(?string $value, bool $required): void
+    {
+        $this->expectNotToPerformAssertions($value, $required);
     }
 }
